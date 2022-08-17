d828d41bdd373146148245eaffcc81c86132595e
huseyin zengin <hzengin@genbil.com>
2022-08-17 18:55:02 +0300

    add project files

---
+++ CallCloudFlowInPlugin/ValCreateCheckProjectName.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace CallCloudFlowInPlugin
+{
+    public class ValCreateCheckProjectName : IPlugin
+    {
+        IOrganizationService service;
+        IPluginExecutionContext context;
+        ITracingService trace;
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                Initial(serviceProvider);
+
+                var entity = (Entity)context.InputParameters["Target"];
+                new ValCreateCheckProjectNameHandler(service).Handle(entity);
+            }
+            catch (Exception ex)
+            {
+                trace.Trace(ex.Message);
+                throw new InvalidPluginExecutionException($"Execuion of {typeof(ValCreateCheckProjectName).Name} | Message: {ex.Message}", ex);
+            }
+        }
+
+        private void Initial(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                service = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(context.UserId);
+                trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException("GetOrganizationService Error", ex);
+            }
+        }
+    }
+}
---
+++ CallCloudFlowInPlugin/ValCreateCheckProjectNameHandler.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xrm.Sdk;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CallCloudFlowInPlugin
+{
+    internal class ValCreateCheckProjectNameHandler
+    {
+        IOrganizationService service;
+        public ValCreateCheckProjectNameHandler(IOrganizationService _service)
+        {
+            service = _service;
+        }
+
+        public void Handle(Entity entity)
+        {
+            if (!entity.Contains("hz_name")) return;
+
+            //Check(entity);
+            CheckWithFlow(entity);
+        }
+
+        public void Check(Entity entity)
+        {
+            if (entity.GetAttributeValue<string>("hz_name") == "xyz")
+                throw new InvalidPluginExecutionException($"Invalid project name ({entity.GetAttributeValue<string>("hz_name")})");
+        }
+
+        public void CheckWithFlow(Entity entity)
+        {
+            var projectName = entity.GetAttributeValue<string>("hz_name");
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://prod-49.westeurope.logic.azure.com/workflows/55dfb05649164970.............");
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/json";
+
+            var data = Serializer.Serialize<PostRequest>(new PostRequest { ProjectName = projectName });
+            var dataContent = Encoding.UTF8.GetBytes(data);
+            using (var stream = webRequest.GetRequestStream())
+            {
+                stream.Write(dataContent, 0, dataContent.Length);
+            }
+
+            Stream response = ((HttpWebResponse)webRequest.GetResponse()).GetResponseStream();
+            string result;
+            using (var sr = new StreamReader(response))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            var post = Serializer.Deserialize<PostResponse>(result);
+            if (post.IsValid == "invalid")
+                throw new InvalidPluginExecutionException($"(Flow) Invalid project name ({projectName})");
+
+        }
+    }
+
+    [DataContract]
+    public class PostRequest
+    {
+        [DataMember(Name = "projectName")]
+        public string ProjectName { get; set; }
+    }
+
+    [DataContract]
+    public class PostResponse
+    {
+        [DataMember(Name = "isValid")]
+        public string IsValid { get; set; }
+    }
+}
